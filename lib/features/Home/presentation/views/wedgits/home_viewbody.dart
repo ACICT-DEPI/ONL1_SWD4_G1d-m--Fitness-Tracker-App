af59b1ec59577b9cbee77b761155964de5d46d74
Hesham Mohamed <heshamffgg@gmail.com>
2024-09-27 15:35:48 +0300

    homefolders

---
+++ lib/features/Home/presentation/views/wedgits/home_viewbody.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeViewbody extends StatelessWidget {
+  const HomeViewbody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
