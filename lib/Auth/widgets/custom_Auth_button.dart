a75d4e6260a6f7e76dc0848e95afd0d396af5ec0
mayaralaa25 <100362336+mayaralaa25@users.noreply.github.com>
2024-09-28 18:34:59 +0300

    Auth commit

---
+++ lib/Auth/widgets/custom_Auth_button.dart
@@ -0,0 +1,38 @@
+import 'package:final_project/core/color.dart';
+import 'package:flutter/material.dart';
+
+class CustomAuthButton extends StatelessWidget {
+  CustomAuthButton({super.key,
+    required this.text,
+    required this.onPressed
+  });
+
+  late String text;
+  void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 18.0),
+      child: ElevatedButton(
+          onPressed: onPressed,
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10,
+                horizontal: MediaQuery.of(context).size.width / 4),
+            child: Text(text,
+              style: TextStyle(
+                  color: Colors.white,
+                fontSize: 20
+              ),),
+          ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: primary,
+          elevation: 4,
+            shape:  const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+        ),
+      ),
+    );
+  }
+}
