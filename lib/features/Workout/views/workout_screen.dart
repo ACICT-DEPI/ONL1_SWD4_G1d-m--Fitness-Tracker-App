62650b89738f869509d6d5c47fa20d0bdd14330f
Hesham Mohamed <heshamffgg@gmail.com>
2024-10-21 07:01:40 +0300

    .

---
+++ lib/features/Workout/views/workout_screen.dart
@@ -0,0 +1,11 @@
+import 'package:final_project/features/Workout/presentation/views/widgets/workout_screen_body.dart';
+import 'package:flutter/material.dart';
+
+class WorkoutScreen extends StatelessWidget {
+  const WorkoutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const WorkoutScreenBody();
+  }
+}
